c45edce0bf022b812d598bec924d584c7a7e20b8
Arvind Yadav <arvind.yadav@intel.com>
2026-07-14 12:14:02 +0530

    drm/xe/guc: Keep scheduler timeline name alive
    The scheduler keeps a pointer to the timeline name, but q->name
    is freed with the exec queue while scheduler fences can still
    reference it.
    
    Store the name in struct xe_guc_exec_queue so it shares
    the scheduler's RCU-deferred lifetime.
    
    Fixes: 6bd90e700b42 ("drm/xe: Make dma-fences compliant with the safe access rules")
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Signed-off-by: Arvind Yadav <arvind.yadav@intel.com>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Acked-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patch.msgid.link/20260714064402.2457257-1-arvind.yadav@intel.com
    Signed-off-by: Tejas Upadhyay <tejas.upadhyay@intel.com>
    (cherry picked from commit 41075f0eb5dcbd3b065d15f15ef7bbe9315188e8)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    
    (cherry picked from commit 299bc6d50b1bed7d1f408391736712f01a0855e2)
    [datiscum: adapt scheduler initialization to the Linux 6.18 interfaces]

--- drivers/gpu/drm/xe/xe_guc_submit.c
+++ drivers/gpu/drm/xe/xe_guc_submit.c
@@ -1715,11 +1715,12 @@ static int guc_exec_queue_init(struct xe_exec_queue *q)
 		goto err_free;
 
 	xe_exec_queue_assign_name(q, q->guc->id);
+	strscpy(ge->name, q->name, sizeof(ge->name));
 
 	err = xe_sched_init(&ge->sched, &drm_sched_ops, &xe_sched_ops,
 			    NULL, q->lrc[0]->ring.size / MAX_JOB_SIZE_BYTES, 64,
 			    timeout, guc_to_gt(guc)->ordered_wq, NULL,
-			    q->name, gt_to_xe(q->gt)->drm.dev);
+			    ge->name, gt_to_xe(q->gt)->drm.dev);
 	if (err)
 		goto err_release_id;
 
